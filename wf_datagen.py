3bd011a2a9102e58fc779dd4947c847323f0f982
Trenton Mosher <starkbewegung@hotmail.com>
2024-10-20 21:02:30 -0700

    All data files download properly

--- wf_datagen.py
+++ wf_datagen.py
@@ -2,26 +2,67 @@
 Author: Trenton Mosher
 Description: Acquires data from internet and unpacks it in local storage.
 """
-
 # site url for download is https://zenodo.org/records/4557828/files/covid-misinfo-videos.csv.gz?download=1
 
-def get_data(url):
+"""Site URLs for misinfo summary data 
+1. https://osf.io/download/6pdxw/
+2. https://osf.io/download/6cj7t/
+3. https://osf.io/download/7zshu/
+4. https://osf.io/download/2aqhj/
+"""
+
+import os
+
+current_dir = os.getcwd()
+
+
+def get_data_cvd_misinfo(url):
     import requests
     response = requests.get(url)
     if response.status_code == 200:
-        import os
-        os.makedirs("/data_original")
-        with response.content as f:
-            import shutil
-            print(f"{f}")
-            try:
-                shutil.move(f"/{f}", f"/data_original/{f}")
-                shutil.unpack_archive(f"/data_original/{f}", f"/data_original/{f}")
-            except:
-                pass
+        import gzip
+        if not os.path.isdir(current_dir + "/data_original"):
+            os.makedirs(current_dir + "/data_original")
+        # save response content to file
+        with open(current_dir + "/data_original/cvd_misinfo_data.csv.gz", 'wb') as f:
+            f.write(response.content)
+        with open(current_dir + "/data_original/cvd_misinfo_data.csv.gz", 'rb') as f:
+            file_content = gzip.decompress(f.read())
+        with open (current_dir + "/data_original/cvd_misinfo_data.csv", 'wb') as f:
+            f.write(file_content)
+        print("Finished COVID misinfo download")
     else:
-        print("Unsuccessful response code received")
+        print("URL download request unsuccessful.")
+
+def get_data_sharing_misinfo(url, val):
+    # most of this code will be very similar until reading of the data
+    import requests
+    response = requests.get(url)
+    if response.status_code == 200:
+        import pandas as pd
+        import pyreadstat
+        if not os.path.isdir(current_dir + "/data_original"):
+            os.makedirs(current_dir + "/data_original")
+        #download the original file as a .sav (we can keep it, but don't really need it)
+        with open(current_dir + f"/data_original/misinfo_study{val}.sav", 'wb') as f:
+            f.write(response.content)
+        #convert to a .csv for actual use later (mostly because I just don't want to learn a new format)
+        file_content = pd.read_spss(current_dir + f"/data_original/misinfo_study{val}.sav")
+        with open(current_dir + f"/data_original/misinfo_study{val}.csv", 'wb') as f:
+            file_content.to_csv(f, index=False)
+        print(f"Finished file {val}")
+    else:
+        print("URL download request unsuccessful.")
 
 if __name__ == "__main__":
-    url = "https://zenodo.org/records/4557828/files/covid-misinfo-videos.csv.gz?download=1"
-    get_data(url)
+    # download and process all files
+    cvd_misinfo_url = "https://zenodo.org/records/4557828/files/covid-misinfo-videos.csv.gz?download=1"
+    get_data_cvd_misinfo(cvd_misinfo_url)
+    osf_summ_data_1 = "https://osf.io/download/6pdxw/"
+    get_data_sharing_misinfo(osf_summ_data_1, 1)
+    osf_summ_data_2 = "https://osf.io/download/6cj7t/"
+    get_data_sharing_misinfo(osf_summ_data_2, 2)
+    osf_summ_data_3 = "https://osf.io/download/7zshu/"
+    get_data_sharing_misinfo(osf_summ_data_3, 3)
+    osf_summ_data_4 = "https://osf.io/download/2aqhj/"
+    get_data_sharing_misinfo(osf_summ_data_4, 4)
