73af1a55f0ff7f5f0df4b8d87b30c4b41bda3675
Trenton Mosher <starkbewegung@hotmail.com>
2024-11-25 13:25:26 -0700

    split training is done

--- wf_ml_evaluation.py
+++ wf_ml_evaluation.py
@@ -2,7 +2,7 @@
 Author: Trenton Mosher
 Description: Splits the data into two sets, trains the model, and evaluates it
 """
-from tabnanny import check
+import os
 
 
 def split_training(filepath, percent=0.8):
@@ -16,3 +16,18 @@ def split_training(filepath, percent=0.8):
     # load csv file into memory, split, and return both data structures as a tuple
     from wf_visualization import check_files
     csv_data = check_files("processed_misinfo_sharing_combined.csv")
+    split_ind = int(len(csv_data) * percent)
+    return csv_data[split_ind:], csv_data[:split_ind]
+
+def get_processed_data(filename):
+    import csv
+    if not os.path.isdir(os.getcwd() + "\\data_processing") or not os.path.isfile(
+            os.getcwd() + f"\\data_processing\\{filename}"):
+        print("Failed to find processed data files")
+        return None
+    csv_data = []
+    # extract csv data
+    with open(os.getcwd() + f"\\data_processing\\{filename}", "r") as file:
+        csv_reader = csv.reader(file)
+        [csv_data.append(row) for row in csv_reader]
+    return [int(col) for val in csv_data for col in val]
